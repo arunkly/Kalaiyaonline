c0eaf1ce6401f83d8f85af5a67be9875e9b81dad
arunkly <arunkly@gmail.com>
2026-09-10 18:21:06 +0000

    Serve robots.txt from SEO settings

---
+++ src/routes/robots[.]txt.ts
@@ -0,0 +1,26 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { getSeoSettings } from "@/lib/seo";
+
+export const Route = createFileRoute("/robots.txt")({
+  server: {
+    handlers: {
+      GET: async () => {
+        const seo = await getSeoSettings();
+        const host = seo.canonicalUrl.replace(/\/$/, "");
+        const body = [
+          "User-agent: *",
+          seo.robots.includes("noindex") ? "Disallow: /" : "Allow: /",
+          "Disallow: /admin",
+          "Disallow: /api/",
+          "Disallow: /login",
+          "Disallow: /account",
+          `Sitemap: ${host}/sitemap.xml`,
+          "",
+        ].join("\n");
+        return new Response(body, {
+          headers: { "Content-Type": "text/plain; charset=utf-8", "Cache-Control": "public, max-age=3600" },
+        });
+      },
+    },
+  },
+});
